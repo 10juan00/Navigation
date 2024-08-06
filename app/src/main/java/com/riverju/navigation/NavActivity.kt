36f930b2b7c438a02ff1e93878e842ba0dd4287e
Riverju <10juan00@gmail.com>
2024-08-06 11:01:55 -0600

    Segunda versión //Menu Toolbar, Eliminando Navigation Drawer Solo Settigns //Bottom Navigation View //

--- app/src/main/java/com/riverju/navigation/NavActivity.kt
+++ app/src/main/java/com/riverju/navigation/NavActivity.kt
@@ -4,23 +4,16 @@ package com.riverju.navigation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.core.view.GravityCompat
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.navigation.NavigationView
-import androidx.appcompat.app.ActionBarDrawerToggle
 import android.view.Menu
 import android.view.MenuItem
 
-class NavActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class NavActivity : AppCompatActivity() {
     // Fragmentos que se cargarán en la actividad
     private val firstFragment = FirstFragment()
     private val secondFragment = SecondFragment()
     private val thirdFragment = ThirdFragment()
-
-    // DrawerLayout para el Navigation Drawer
-    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,17 +23,6 @@ class NavActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelected
         // Configuración de la Toolbar
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
-
-        // Inicialización del DrawerLayout y NavigationView
-        drawerLayout = findViewById(R.id.drawer_layout)
-        val navigationView: NavigationView = findViewById(R.id.navigation_view)
-        navigationView.setNavigationItemSelectedListener(this)
-
-        // Configuración del ActionBarDrawerToggle para manejar la apertura y cierre del drawer
-        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar,
-            R.string.navigation_drawer_open, R.string.navigation_drawer_close)
-        drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
 
         // Inicialización del BottomNavigationView y su listener
         val navigation: BottomNavigationView = findViewById(R.id.bottom_navigation)
@@ -60,6 +42,14 @@ class NavActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelected
         // Manejar los clics en los elementos del menú de opciones
         return when (item.itemId) {
             R.id.action_settings -> {
+                // Acción al seleccionar 'settings'
+                true
+            }
+            R.id.nav_refresh -> {
+                // Acción al seleccionar 'settings'
+                true
+            }
+            R.id.nav_logout -> {
                 // Acción al seleccionar 'settings'
                 true
             }
@@ -93,31 +83,4 @@ class NavActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelected
         transaction.replace(R.id.frame_container, fragment)
         transaction.commit()
     }
-
-    // Manejar los clics en los ítems del Navigation Drawer
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.nav_profile -> {
-                // Acción al seleccionar 'profile'
-            }
-            R.id.nav_info -> {
-                // Acción al seleccionar 'info'
-            }
-            R.id.nav_logout -> {
-                // Acción al seleccionar 'logout'
-            }
-        }
-        drawerLayout.closeDrawer(GravityCompat.START)
-        return true
-    }
-
-    // Manejar el comportamiento del botón de retroceso
-    override fun onBackPressed() {
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            drawerLayout.closeDrawer(GravityCompat.START)
-        } else {
-            super.onBackPressed()
-        }
-    }
 }
-
